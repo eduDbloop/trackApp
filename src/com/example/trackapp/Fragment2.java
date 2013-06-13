63654721302259a08f86c3529746a41443050a37
Eduardo Melgarejo <emelgarejo0411@gmail.com>
2013-06-13 10:08:38 +0200

    First revision from Ger.

--- src/com/example/trackapp/Fragment2.java
+++ src/com/example/trackapp/Fragment2.java
@@ -3,13 +3,18 @@ package com.example.trackapp;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class Fragment2 extends SherlockFragment{
+public class Fragment2 extends SherlockFragment{	
 	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){	
 		return inflater.inflate(R.layout.fragment_2, container, false);
 	}
 
--- src/com/example/trackapp/MainActivity.java
+++ src/com/example/trackapp/MainActivity.java
@@ -13,9 +13,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends SherlockFragmentActivity {
 
+	
+	SharedPreferences mPrefs;
+	final String welcomeScreenShownPref = "welcomeScreenShown";
+	
 	private ViewPager mViewPager;
 	private TabsAdapter mTabsAdapter;
 	
@@ -23,10 +29,39 @@ public class MainActivity extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 				
+		//ImageButton im = (ImageButton) findViewById(R.id.sosButton);
 		mViewPager = new ViewPager(this);
 		mViewPager.setId(R.id.pager);
 		setContentView(mViewPager);
 		
+		/*im.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(MainActivity.this,PanicWindow.class);
+				MainActivity.this.startActivity(myIntent);
+			}
+		});*/
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		// Second argument is the default to use if the preference can't be found
+		Boolean welcomeScreenShown = mPrefs.getBoolean(welcomeScreenShownPref, false);
+		
+		if (!welcomeScreenShown) {
+			
+			String whatsNewTitle = getResources().getString(R.string.whatsNewTitle);
+		        String whatsNewText = getResources().getString(R.string.whatsNewText);
+		        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle(whatsNewTitle).setMessage(whatsNewText).setPositiveButton(
+		                R.string.ok, new DialogInterface.OnClickListener() {
+		                    public void onClick(DialogInterface dialog, int which) {
+		                        dialog.dismiss();
+		                    }
+		                }).show();
+		        SharedPreferences.Editor editor = mPrefs.edit();
+		        editor.putBoolean(welcomeScreenShownPref, true);
+		        editor.commit(); // Very important to save the preference
+		} 
+			 
 		final ActionBar bar = getSupportActionBar();
 		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
@@ -35,5 +70,8 @@ public class MainActivity extends SherlockFragmentActivity {
 		mTabsAdapter.addTab(bar.newTab().setText("Alerta"), Fragment2.class, null);
 		mTabsAdapter.addTab(bar.newTab().setText("Datos"), FragmentLayout.class, null);
 	    
+	
+	
 	}
+	
 }
